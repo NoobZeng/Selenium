be473561ca9bdef3806b32178ca1fa39a2f5fc4c
noobzeng <853923850@qq.com>
2018-12-12 16:03:07 +0800

    appium测试真机移动端web

---
+++ chapter07/android_web_simulator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#! _*_ coding:utf-8 _*_
+# @TIME   : 2018/12/3  23:43
+# @Author : Noob
+# @File   : android_web_actual.py
+
+from selenium import webdriver
+import unittest
+import time
+import warnings
+
+class AndroidWeb(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter('ignore', ResourceWarning)
+        print('start')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        desired_caps = dict()
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '4.4.2'
+        desired_caps['deviceName'] = '127.0.0.1:62001'
+        desired_caps['browserName'] = 'Chrome'
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_caps, options=chrome_options)
+        self.driver.get('http://weibo.com')
+        time.sleep(10)
+
+    def test_web(self):
+        print('test……')
+
+    def tearDown(self):
+        print('end')
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
