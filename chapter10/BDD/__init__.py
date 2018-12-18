faf862ddc99fa33a764563ff6eb4ad758a553fe1
noobzeng <853923850@qq.com>
2018-12-18 14:16:17 +0800

    初始化文件

---
+++ chapter10/BDD/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python
+# ! _*_ coding:utf-8 _*_
+# @TIME   : 2018/12/17  20:51
+# @Author : Noob
+# @File   : __init__.py.py
---
+++ chapter10/Jenkins/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python
+# ! _*_ coding:utf-8 _*_
+# @TIME   : 2018/12/18  1:27
+# @Author : Noob
+# @File   : __init__.py.py
